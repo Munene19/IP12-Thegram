c5fa39dfcd994b7586369c1a2b359526bdb6f348
munene19 <munenexv@gmail.com>
2020-02-11 15:51:30 +0300

    Initial commit

--- gram/models.py
+++ gram/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    profile_photo = models.ImageField()
+    Bio = models.CharField(max_length=40)
+
+class Image(models.Model):
+    image = models.ImageField()
+    image_caption = models.TextField(max_length=40)
+    user= models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+    likes = models.IntegerField(default=0, null=True)
+    comments = models.CharField(max_length=20)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile
+from .forms import *
 
-# Create your views here.
+def signup(request):
+    context = {}
+    return render(request, 'sign-up.html', context)
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
