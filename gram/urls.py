eabd197d6af46072c66a74542ef51651d4d6ac09
munene19 <munenexv@gmail.com>
2020-02-13 04:48:29 +0300

    urls added

--- gram/urls.py
+++ gram/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     
     url(r'^', views.index, name='index'),
     url(r'^/sign-up/', views.signup)
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
