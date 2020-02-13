53480eff0f6099b974371f0c3ef33cae637dfaaf
munene19 <munenexv@gmail.com>
2020-02-13 07:38:58 +0300

    thegram urls updated

--- thegram/urls.py
+++ thegram/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
+from registration.backends.simple.views import RegistrationView
+from gram.forms import UserRegisterForm
 
 
 urlpatterns = [
