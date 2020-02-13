a873be440425349e60a89aae8aa3f90367c1ebe3
munene19 <munenexv@gmail.com>
2020-02-13 07:25:25 +0300

    home view updated

--- gram/urls.py
+++ gram/urls.py
@@ -7,7 +7,13 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     url(r'^', views.index, name='index'),
-    url(r'^/sign-up/', views.signup)
+    url(r'^/sign-up/', views.signup),
+    url(r'^$', views.home, name= 'home'),
+    url(r'^search/', views.search_images, name='search_results'),
+    url(r'^image/(\d+)', views.get_image, name='image_results'),
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    # url(r'^like/(\d+)', views.like_image, name='like_image'),
 ]
 
 
